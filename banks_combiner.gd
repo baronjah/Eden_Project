a3d94d889e2b6edda0fefe8de5570346bc36bada
baronjah <65126699+baronjah@users.noreply.github.com>
2025-01-27 19:37:27 +0100

    Create banks_combiner.gd
    banks combiner, when i made more than one at first, i was like damn, i already copy pasted so i will keep on doing it, later when i rewritten many things from start to finish, that number in array saved me xD

---
+++ banks_combiner.gd
@@ -0,0 +1,100 @@
+#
+# JSH Ethereal Banks Combiner
+#
+#      oooo  .oooooo..o ooooo   ooooo 
+#      `888 d8P'    `Y8 `888'   `888' 
+#       888 Y88bo.       888     888     ┏┓ ┓         ┓  ┳┓    ┓    ┏┓     ┓ •      
+#       888  `"Y8888o.   888ooooo888     ┣ ╋┣┓┏┓┏┓┏┓┏┓┃  ┣┫┏┓┏┓┃┏┏  ┃ ┏┓┏┳┓┣┓┓┏┓┏┓┏┓
+#       888      `"Y88b  888     888     ┗┛┗┛┗┗ ┛ ┗ ┗┻┗  ┻┛┗┻┛┗┛┗┛  ┗┛┗┛┛┗┗┗┛┗┛┗┗ ┛
+#       888 oo     .d8P  888     888          
+#   .o. 88P 8""88888P'  o888o   o888o 
+#   `Y888P                            
+#
+# JSH Ethereal Banks Combiner
+#
+
+extends Node3D
+class_name BanksCombiner #BanksCombiner.combination_0
+
+
+# BanksCombiner.data_names_0
+const data_names_0 = [
+	"records", "instructions", "scenes", "interactions"
+]
+
+const data_names_1 = [
+	"records"
+]
+
+const data_names_2 = [
+	"instructions", "scenes", "interactions"
+]
+
+const data_sets_names = [
+	"base_", "menu_", "settings_", "keyboard_", "keyboard_left_", "keyboard_right_", "things_creation_", "singular_lines_"
+]
+
+const dataSetLimits = { # dataSetLimits and data_sets_names in BanksCombiner
+	"base_": 1,
+	"menu_": 1,
+	"settings_": 1,
+	"keyboard_": 1,
+	"keyboard_left_": 1,
+	"keyboard_right_": 1,
+	"things_creation_": 2,
+	"singular_lines_": 8
+}
+
+
+# the new way to do things
+const combination_new_gen_0 = [
+	[0]
+]
+
+
+# the new way to do things
+const combination_new_gen_1 = [
+	[1], [2], [3]
+]
+
+
+
+# the base one
+const combination_0 = [
+	[0], [1], [2], [3]
+]
+
+# the menu one
+const combination_1 = [
+	[0], [1], [2], [3]
+]
+
+# the settings one
+const combination_2 = [
+	[0], [1], [2], [3]
+]
+
+# the keyboard bracekt one
+const combination_3 = [
+	[0], [1], [2], [3]
+]
+
+# keyboard left
+const combination_4 = [
+	[0], [1], [2], [3]
+]
+
+# keyboard migi
+const combination_5 = [
+	[0], [1], [2], [3]
+]
+
+# things creation
+const combination_6 = [
+	[0], [1], [2], [3]
+]
+
+# things creation
+const combination_7 = [
+	[0], [1], [2], [3]
+]
